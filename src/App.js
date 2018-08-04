3256073b3ffda09096998bff5baf1d5a018e6e3e
wasib <mohammedwasib@gmail.com>
2018-08-05 00:43:59 +0530

    app fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import MenuComplete from "./Components/MenuComplete";
 import { Home } from "./Components/Home";
 import { Contact } from "./Components/Contact";
