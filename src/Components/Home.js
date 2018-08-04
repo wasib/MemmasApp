da8b32cd44831f5a6bbf4e8f9c98b84fa28d4b17
wasib <mohammedwasib@gmail.com>
2018-08-05 00:35:14 +0530

    fixed home bug

---
+++ src/Components/Home.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+      <h1>I am Home</h1>
+    </div>
+  );
+};
