c9496d94c063591add393082b9cbf1f69a870182
wasib <mohammedwasib@gmail.com>
2018-08-04 16:02:34 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
 import { Button,Icon } from 'semantic-ui-react'
 
 class App extends Component {
