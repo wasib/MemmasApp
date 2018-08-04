d899c4ca4ef7d0936161d49869f5dd4479608998
wasib <mohammedwasib@gmail.com>
2018-08-04 22:19:42 +0530

    pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, { Component } from "C:/Users/wasib/AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/react";
 import MenuComplete from "./Components/MenuComplete";
-import {Home} from './Components/home'
+import {Home} from './Components/Home'
 import {Footer} from './Components/footer'
 import logo from "./logo.svg";
 import "./styles/App.css";
@@ -24,7 +24,12 @@ class App extends Component {
     return (
       <div>
         <MenuComplete activeItem={this.state.activeItem} changeActiveItem={this.changeActiveItem} />
-        <Home />
+        <Router>
+             <div>
+               <Route exact path="/" component={Home} />
+               <Route path="/attendance" component={Attendance} />
+               </div>  
+          </Router>
         <Footer />
       </div>
     );
