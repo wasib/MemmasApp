f8c4885ccc38f3a54ea3eeccd47b17a7c1e91722
wasib <mohammedwasib@gmail.com>
2018-08-14 02:14:05 +0530

    contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../styles/Contact.css";
-import { Header, List, Container } from "semantic-ui-react";
 import ContactForm from "./ContactForm";
 
 export class Contact extends React.Component {
@@ -16,29 +15,27 @@ export class Contact extends React.Component {
         </div>
 
         <div className="middle-contact">
-          <div classname="left-contact-info">
-            <Container textAlign="justified">
-              <Header as="h2">Contact Info:</Header>
-              <p style={{ padding: "0px 15px 0px 15px" }}>
-                Domestic dogs inherited complex behaviors, such as bite
-                inhibition, from their wolf ancestors, which would have been
-                pack hunters with complex body language.
-              </p>
-              <List>
-                <List.Item>
-                  <b>Address:</b> 10111 Santa Monica Boulevard, LA
-                </List.Item>
-                <List.Item>
-                  <b>Phone:</b> +44 987 065 908
-                </List.Item>
-                <List.Item>
-                  <b>Email:</b> info@example.com
-                </List.Item>
-                <List.Item>
-                  <b>Fax:</b> +44 987 065 909
-                </List.Item>
-              </List>
-            </Container>
+          <div className="left-contact-info">
+            <h2>Contact Info:</h2>
+            <p>
+              Domestic dogs inherited complex behaviors, such as bite
+              inhibition, from their wolf ancestors, which would have been pack
+              hunters with complex body language.
+            </p>
+            <ul>
+              <li>
+                <b>Address:</b> 10111 Santa Monica Boulevard, LA
+              </li>
+              <li>
+                <b>Phone:</b> +44 987 065 908
+              </li>
+              <li>
+                <b>Email:</b> info@example.com
+              </li>
+              <li>
+                <b>Fax:</b> +44 987 065 909
+              </li>
+            </ul>
           </div>
           <div className="right-contact-form">
             <ContactForm />
