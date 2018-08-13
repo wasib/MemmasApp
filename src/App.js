7af7ed7fa7d75397f0b32b81df00a675689f0f0b
Mohammad Aasif <31933645+mdasif312@users.noreply.github.com>
2018-08-14 01:46:48 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import { Menu, Icon, Segment, Sidebar } from "semantic-ui-react";
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+import {Menu, Icon, Segment, Sidebar} from "semantic-ui-react";
 import AppBar from "./components/AppBar";
-import { Home } from "./components/Home";
-import { Contact } from "./components/Contact";
-import { About } from "./components/About";
-import { Product } from "./components/Product";
+import {Home} from "./components/Home";
+import {Contact} from "./components/Contact";
+import {About} from "./components/About";
+import {Product} from "./components/Product";
 import ProductView from "./components/ProductView";
-import { Services } from "./components/Services";
-import { Footer } from "./components/Footer";
-import { PageNotFound } from "./components/PageNotFound";
+import {Services} from "./components/Services";
+import {Footer} from "./components/Footer";
+import {PageNotFound} from "./components/PageNotFound";
 import "./styles/App.css";
 
 class App extends Component {
@@ -18,90 +18,95 @@ class App extends Component {
     this.state = {
       visible: false
     };
-    this.handleSidebarHide = this.handleSidebarHide.bind(this);
-    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.handleButtonClick = this
+      .handleButtonClick
+      .bind(this);
+
+    this.closeNavBar = this
+      .closeNavBar
+      .bind(this);
+    this.redirect = this
+      .redirect
+      .bind(this);
   }
 
   handleButtonClick = () => {
     window.scrollTo(0, 0);
-    this.setState({ visible: !this.state.visible });
+    this.setState({
+      visible: true //!this.state.visible
+    });
   };
 
-  handleSidebarHide = () => this.setState({ visible: false });
+  closeNavBar = () => this.setState({visible: false});
+  redirect = (target) => {
+    this.closeNavBar;
+    window.location = "/" + target;
+    /*
+    this
+      .props
+      .history
+      .push("/" + target);
+      */
+  }
 
   render() {
-    const { visible } = this.state;
+    console.log("render called");
+    const {visible} = this.state;
 
     return (
-      <Router>
-        <div>
-          <AppBar handleButtonClick={this.handleButtonClick} />
+      <div>
+        <Router>
+          <div>
+            <AppBar handleButtonClick={this.handleButtonClick}/>
 
-          <div style={{ marginTop: "60px" }}>
-            <Sidebar.Pushable as={Segment}>
-              <Sidebar
-                as={Menu}
-                animation="overlay"
-                direction="right"
-                icon="labeled"
-                inverted
-                onHide={this.handleSidebarHide}
-                vertical
-                visible={visible}
-                width="thin"
-              >
-                <Link to="/">
-                  <Menu.Item>
-                    <Icon name="home" />
-                    Home
-                  </Menu.Item>
-                </Link>
-                <Link to="/About">
-                  <Menu.Item>
-                    <Icon name="info" />
-                    About
-                  </Menu.Item>
-                </Link>
-                <Link to="/Product">
-                  <Menu.Item>
-                    <Icon name="camera" />
-                    Product
-                  </Menu.Item>
-                </Link>
-                <Link to="/Services">
-                  <Menu.Item>
-                    <Icon name="settings" />
-                    Services
-                  </Menu.Item>
-                </Link>
-                <Link to="/Contact">
-                  <Menu.Item>
-                    <Icon name="conversation" />
-                    Contact
-                  </Menu.Item>
-                </Link>
-              </Sidebar>
+            <div style={{
+              marginTop: "60px"
+            }}>
+              <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route path="/Home" component={Home}/>
+                <Route path="/About" component={About}/>
+                <Route exact path="/Product" component={Product}/>
+                <Route path="/Product/:id" component={ProductView}/>
+                <Route path="/Services" component={Services}/>
+                <Route path="/Contact" component={Contact}/>
+                <Route component={PageNotFound}/>
+              </Switch>
 
-              <Sidebar.Pusher dimmed={visible}>
-                <Segment basic>
-                  <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/Home" component={Home} />
-                    <Route path="/About" component={About} />
-                    <Route exact path="/Product" component={Product} />
-                    <Route path="/Product/:id" component={ProductView} />
-                    <Route path="/Services" component={Services} />
-                    <Route path="/Contact" component={Contact} />
-                    <Route component={PageNotFound} />
-                  </Switch>
-                </Segment>
-              </Sidebar.Pusher>
-            </Sidebar.Pushable>
+            </div>
+
+            <Footer/> {console.log(visible)}
+            {visible
+              ? <div className="sidenav">
+                  <a href="javascript:void(0)" className="closebtn" onClick={this.closeNavBar}>&times;</a>
+
+                  <span
+                    onClick={() => {
+                    this.redirect("")
+                  }}>Home</span>
+                  <span
+                    onClick={() => {
+                    this.redirect("About")
+                  }}>About</span>
+                  <span
+                    onClick={() => {
+                    this.redirect("Product")
+                  }}>Product</span>
+                  <span
+                    onClick={() => {
+                    this.redirect("Services")
+                  }}>Services</span>
+                  <span
+                    onClick={() => {
+                    this.redirect("Contact")
+                  }}>Contact</span>
+                  
+                </div>
+              : <div/>}
+
           </div>
-
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </div>
     );
   }
 }
