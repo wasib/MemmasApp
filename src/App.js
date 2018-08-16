71dfd9b5458e10ce364863655205a16f7371088e
wasib <mohammedwasib@gmail.com>
2018-08-17 02:19:01 +0530

    icons in menu

--- src/App.js
+++ src/App.js
@@ -1,79 +1,77 @@
-import React, {Component} from "react";
-import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
-import {Menu, Icon, Segment, Sidebar} from "semantic-ui-react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AppBar from "./components/AppBar";
-import {Home} from "./components/Home";
-import {Contact} from "./components/Contact";
-import {About} from "./components/About";
-import {Product} from "./components/Product";
+import { Home } from "./components/Home";
+import { Contact } from "./components/Contact";
+import { About } from "./components/About";
+import { Product } from "./components/Product";
 import ProductView from "./components/ProductView";
-import {Services} from "./components/Services";
-import {Footer} from "./components/Footer";
-import {PageNotFound} from "./components/PageNotFound";
+import { Services } from "./components/Services";
+import { Footer } from "./components/Footer";
+import { PageNotFound } from "./components/PageNotFound";
 import "./styles/App.css";
-import { withRouter } from "react-router-dom";
 import MobileNav from "./components/MobileNav";
 
 class App extends Component {
   constructor(props) {
     super(props);
-     this.handleButtonClick = this
-      .handleButtonClick
-      .bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
 
-      this.state = {
-        mobileNavBarVisible: false
-      }
-      
-      this.redirect = this.redirect.bind(this);
-      this.closeMobileNavBar = this.closeMobileNavBar.bind(this); 
+    this.state = {
+      mobileNavBarVisible: false
+    };
 
-      this.child = React.createRef();
+    this.redirect = this.redirect.bind(this);
+    this.closeMobileNavBar = this.closeMobileNavBar.bind(this);
 
-    }
+    this.child = React.createRef();
+  }
 
   handleButtonClick = () => {
     window.scrollTo(0, 0);
     this.setState({
-      mobileNavBarVisible: true 
+      mobileNavBarVisible: true
     });
   };
- 
-  closeMobileNavBar(){
+
+  closeMobileNavBar() {
     this.setState({
       mobileNavBarVisible: false
     });
   }
 
-  redirect(target){
+  redirect(target) {
     this.child.current.redirect(target);
   }
   render() {
-
     return (
       <div>
         <Router>
           <div>
-            <AppBar handleButtonClick={this.handleButtonClick}/>
+            <AppBar handleButtonClick={this.handleButtonClick} />
 
-            <div style={{
-              marginTop: "60px"
-            }}>
+            <div
+              style={{
+                marginTop: "60px"
+              }}
+            >
               <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route path="/Home" component={Home}/>
-                <Route path="/About" component={About}/>
-                <Route exact path="/Product" component={Product}/>
-                <Route path="/Product/:id" component={ProductView}/>
-                <Route path="/Services" component={Services}/>
-                <Route path="/Contact" component={Contact}/>
-                <Route component={PageNotFound}/>
+                <Route exact path="/" component={Home} />
+                <Route path="/Home" component={Home} />
+                <Route path="/About" component={About} />
+                <Route exact path="/Product" component={Product} />
+                <Route path="/Product/:id" component={ProductView} />
+                <Route path="/Services" component={Services} />
+                <Route path="/Contact" component={Contact} />
+                <Route component={PageNotFound} />
               </Switch>
-
             </div>
 
-            <Footer/> 
-            <MobileNav visible={this.state.mobileNavBarVisible} ref = {this.child} closeMobileNavBar = {this.closeMobileNavBar} />
+            <Footer />
+            <MobileNav
+              visible={this.state.mobileNavBarVisible}
+              closeMobileNavBar={this.closeMobileNavBar}
+            />
           </div>
         </Router>
       </div>
--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,121 +1,134 @@
-import React, {Component} from "react";
-import {withRouter} from "react-router-dom";
-import './../styles/MobileNav.css';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import "./../styles/MobileNav.css";
+import { Icon } from "semantic-ui-react";
 
 class MobileNav extends Component {
-  constructor(props) {
-    super(props);
-  }
-
+  
   redirect(target) {
     //alert(target);
-    this
-      .props
-      .history
-      .push("/" + target);
+    this.props.history.push("/" + target);
   }
   render() {
     const visible = this.props.visible;
     //console.log("Duh");
     //console.log(visible);
-    let active = this
-      .props
-      .location
-      .pathname
+    let active = this.props.location.pathname
       .toString()
       .substr(1)
       .trim();
-    if (active.indexOf("/") != -1) 
+    if (active.indexOf("/") !== -1)
       active = active.substr(0, active.indexOf("/"));
-    
+
     return (
       <div
         className="sidenav"
-        style={visible
-        ? {
-          width: '100vw'
+        style={
+          visible
+            ? {
+                width: "100vw"
+              }
+            : {
+                width: "0vw"
+              }
         }
-        : {
-          width: '0vw'
-        }}>
+      >
         <a
           href="javascript:void(0)"
           className="closebtn"
-          onClick={this.props.closeMobileNavBar}>&times;</a>
+          onClick={this.props.closeMobileNavBar}
+        >
+          &times;
+        </a>
 
         <span
           onClick={() => {
-          this
-            .props
-            .closeMobileNavBar();
-          this.redirect("")
-        }}
-          style={active == ""
-          ? {
-            color: '#f1f1f1'
+            this.props.closeMobileNavBar();
+            this.redirect("");
+          }}
+          style={
+            active === ""
+              ? {
+                  color: "#f1f1f1"
+                }
+              : {
+                  color: "#818181"
+                }
           }
-          : {
-            color: '#818181'
-          }}>Home</span>
+        >
+          <Icon name="home" /> Home
+        </span>
         <span
           onClick={() => {
-          this
-            .props
-            .closeMobileNavBar();
-          this.redirect("About")
-        }}
-          style={active == "About"
-          ? {
-            color: '#f1f1f1'
+            this.props.closeMobileNavBar();
+            this.redirect("About");
+          }}
+          style={
+            active === "About"
+              ? {
+                  color: "#f1f1f1"
+                }
+              : {
+                  color: "#818181"
+                }
           }
-          : {
-            color: '#818181'
-          }}>About</span>
+        >
+          <Icon name="info" /> About
+        </span>
         <span
           onClick={() => {
-          this
-            .props
-            .closeMobileNavBar();
-          this.redirect("Product")
-        }}
-          style={active == "Product"
-          ? {
-            color: '#f1f1f1'
+            this.props.closeMobileNavBar();
+            this.redirect("Product");
+          }}
+          style={
+            active === "Product"
+              ? {
+                  color: "#f1f1f1"
+                }
+              : {
+                  color: "#818181"
+                }
           }
-          : {
-            color: '#818181'
-          }}>Product</span>
+        >
+          {" "}
+          <Icon name="camera" /> Product
+        </span>
         <span
           onClick={() => {
-          this
-            .props
-            .closeMobileNavBar();
-          this.redirect("Services")
-        }}
-          style={active == "Services"
-          ? {
-            color: '#f1f1f1'
+            this.props.closeMobileNavBar();
+            this.redirect("Services");
+          }}
+          style={
+            active === "Services"
+              ? {
+                  color: "#f1f1f1"
+                }
+              : {
+                  color: "#818181"
+                }
           }
-          : {
-            color: '#818181'
-          }}>Services</span>
+        >
+          <Icon name="settings" /> Services
+        </span>
         <span
           onClick={() => {
-          this
-            .props
-            .closeMobileNavBar();
-          this.redirect("Contact")
-        }}
-          style={active == "Contact"
-          ? {
-            color: '#f1f1f1'
+            this.props.closeMobileNavBar();
+            this.redirect("Contact");
+          }}
+          style={
+            active === "Contact"
+              ? {
+                  color: "#f1f1f1"
+                }
+              : {
+                  color: "#818181"
+                }
           }
-          : {
-            color: '#818181'
-          }}>Contact</span>
-
+        >
+          <Icon name="conversation" /> Contact
+        </span>
       </div>
-    )
+    );
   }
 }
 
